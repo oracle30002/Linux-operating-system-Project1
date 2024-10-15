ee865ac1656786857840f9baa4ff1d7779eb329b
oracle30002 <160558510+oracle30002@users.noreply.github.com>
2024-10-15 15:54:42 +0800

    Add files via upload

---
+++ Project1/my_get_physical_addresses.c
@@ -0,0 +1,45 @@
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+#include <linux/sched.h>
+#include <asm/pgtable.h>
+
+
+SYSCALL_DEFINE1(my_get_physical_addresses, void *, virt_addr)
+{
+	pgd_t *pgd;
+	p4d_t *p4d;
+	pud_t *pud;
+	pmd_t *pmd;
+	pte_t *pte;
+	unsigned long pfn_addr = 0;
+	unsigned long virt_offset = 0;
+	unsigned long phy_addr = 0;
+	unsigned long vaddr = (unsigned long)virt_addr;
+
+	pgd = pgd_offset(current->mm, vaddr);
+	if (pgd_none(*pgd))
+		return 0;
+	
+	p4d = p4d_offset(pgd, vaddr);
+	if (p4d_none(*p4d))
+		return 0;
+
+	pud = pud_offset(p4d, vaddr);
+	if (pud_none(*pud))
+		return 0;
+
+	pmd = pmd_offset(pud, vaddr);
+	if (pmd_none(*pmd))
+		return 0;
+
+	pte = pte_offset_kernel(pmd, vaddr);
+	if (pte_none(*pte))
+		return 0;
+
+	pfn_addr = pte_val(*pte) & PTE_PFN_MASK & PAGE_MASK;
+	virt_offset = vaddr & ~PAGE_MASK;
+	phy_addr = pfn_addr | virt_offset;
+	
+	
+	return phy_addr;
+}
---
+++ Project1/test2.c
@@ -0,0 +1,26 @@
+ #include <stdio.h>
+                
+
+//void * my_get_physical_addresses(void *);
+
+
+int a[2000000]; 
+                 
+
+int main()
+{ 
+    int      loc_a;
+    void     *phy_add;  
+
+            
+    phy_add=my_get_physical_addresses(&a[0]);
+    printf("global element a[0]:\n", );  
+    prinft("Offest of logical address:[%p]   Physical address:[%p]\n", &a[0], phy_add);              
+    printf("========================================================================\n"); 
+    phy_add=my_get_physical_addresses(&a[1999999]);
+    printf("global element a[1999999]:\n", );  
+    prinft("Offest of logical address:[%p]   Physical address:[%p]\n", &a[1999999], phy_add);              
+    printf("========================================================================\n"); 
+
+
+}
