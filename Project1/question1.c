7da8a1d86e6fc6b6cf73b0c45e28dcb3996f38cb
oracle30002 <160558510+oracle30002@users.noreply.github.com>
2024-10-21 13:06:37 +0800

    Update question1.c

--- Project1/question1.c
+++ Project1/question1.c
@@ -1,8 +1,13 @@
- #include <stdio.h>
-                
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-//void * my_get_physical_addresses(void *);
 
+//void * get_my_physical_addr(void *);
+void *get_my_physical_addr(void *vaddr)
+{
+    return (void *)syscall(449, vaddr);
+}
 
 int global_a=123;  //global variable
 
@@ -18,28 +23,28 @@ int main()
     void     *parent_use, *child_use;  
 
     printf("===========================Before Fork==================================\n");             
-    parent_use=my_get_physical_addresses(&global_a);
+    parent_use=get_my_physical_addr(&global_a);
     printf("pid=%d: global variable global_a:\n", getpid());  
-    prinft("Offest of logical address:[%p]   Physical address:[%p]\n", &global_a,parent_use);              
+    printf("Offest of logical address:[%p]   Physical address:[%p]\n", &global_a,parent_use);              
     printf("========================================================================\n");  
 
     
     if(fork())
     { /*parent code*/
         printf("vvvvvvvvvvvvvvvvvvvvvvvvvv  After Fork by parent  vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n"); 
-        parent_use=my_get_physical_addresses(&global_a);
+        parent_use=get_my_physical_addr(&global_a);
         printf("pid=%d: global variable global_a:\n", getpid()); 
-        prinft("******* Offset of logical address:[%p]   Physical address:[%p]\n", &global_a,parent_use); 
+        printf("******* fset of logical address:[%p]   Physical address:[%p]\n", &global_a,parent_use); 
         printf("vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n");                      
-        wait();                    
+        wait(NULL);                    
     }
     else
     { /*child code*/
 
         printf("llllllllllllllllllllllllll  After Fork by child  llllllllllllllllllllllllllllllll\n"); 
-        child_use=my_get_physical_addresses(&global_a);
+        child_use=get_my_physical_addr(&global_a);
         printf("******* pid=%d: global variable global_a:\n", getpid());  
-        prinft("******* Offset of logical address:[%p]   Physical address:[%p]\n", &global_a, child_use); 
+        printf("******* Offset of logical address:[%p]   Physical address:[%p]\n", &global_a, child_use); 
         printf("llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll\n");  
         printf("____________________________________________________________________________\n");  
 
@@ -47,9 +52,9 @@ int main()
         global_a=789;
 
         printf("iiiiiiiiiiiiiiiiiiiiiiiiii  Test copy on write in child  iiiiiiiiiiiiiiiiiiiiiiii\n"); 
-        child_use=my_get_physical_addresses(&global_a);
+        child_use=get_my_physical_addr(&global_a);
         printf("******* pid=%d: global variable global_a:\n", getpid());  
-        prinft("******* Offset of logical address:[%p]   Physical address:[%p]\n", &global_a, child_use); 
+        printf("******* Offset of logical address:[%p]   Physical address:[%p]\n", &global_a, child_use); 
         printf("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii\n");  
         printf("____________________________________________________________________________\n");                  
         sleep(1000);
