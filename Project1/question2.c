db6be9199f36b7aac8a560e553d4d3c868f54f79
oracle30002 <160558510+oracle30002@users.noreply.github.com>
2024-10-22 11:06:18 +0800

    Update question2.c

--- Project1/question2.c
+++ Project1/question2.c
@@ -1,26 +1,32 @@
- #include <stdio.h>
-                
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
 
 //void * my_get_physical_addresses(void *);
+void *get_my_physical_addr(void *vaddr)
+{
+    return (void *)syscall(449, vaddr);
+}
 
 
-int a[2000000]; 
-                 
+int a[2000000];
+
 
 int main()
-{ 
+{
     int      loc_a;
-    void     *phy_add;  
+    void     *phy_add;
 
-            
-    phy_add=my_get_physical_addresses(&a[0]);
-    printf("global element a[0]:\n", );  
-    prinft("Offest of logical address:[%p]   Physical address:[%p]\n", &a[0], phy_add);              
-    printf("========================================================================\n"); 
-    phy_add=my_get_physical_addresses(&a[1999999]);
-    printf("global element a[1999999]:\n", );  
-    prinft("Offest of logical address:[%p]   Physical address:[%p]\n", &a[1999999], phy_add);              
-    printf("========================================================================\n"); 
+
+    phy_add=get_my_physical_addr(&a[0]);
+    printf("global element a[0]:\n");
+    printf("Offest of logical address:[%p]   Physical address:[%p]\n", &a[0], phy_add);
+    printf("========================================================================\n");
+    phy_add=get_my_physical_addr(&a[1999999]);
+    printf("global element a[1999999]:\n");
+    printf("Offest of logical address:[%p]   Physical address:[%p]\n", &a[1999999], phy_add);
+    printf("========================================================================\n");
 
 
 }
